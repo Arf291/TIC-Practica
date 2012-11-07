c6e169c1f366491926d89a3324bf1800a0aafc24
ALberto <w.whitman22@gmail.com>
2012-11-07 10:33:15 +0100

    Inicio

---
+++ PrácticaTIC/MainMenu.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PrácticaTIC
+{
+    partial class Compressor
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btAction = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lbName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seleccionar archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btAction
+            // 
+            this.btAction.Location = new System.Drawing.Point(115, 133);
+            this.btAction.Name = "btAction";
+            this.btAction.Size = new System.Drawing.Size(75, 23);
+            this.btAction.TabIndex = 1;
+            this.btAction.UseVisualStyleBackColor = true;
+            this.btAction.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(102, 117);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(35, 13);
+            this.lbName.TabIndex = 2;
+            this.lbName.Text = "label1";
+            this.lbName.Visible = false;
+            // 
+            // Compressor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(325, 306);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.btAction);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Compressor";
+            this.Text = "Compressor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btAction;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lbName;
+    }
+}
+
---
+++ PrácticaTIC/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrácticaTIC
+{
+    public partial class Compressor : Form
+    {
+        public Compressor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                String ext = file.FileName.Split('.')[1];
+
+                if (ext == "rar" || ext == "zip")
+                {
+                    btAction.Text = "Descomprimir";
+                }
+                else
+                    btAction.Text = "Comprimir";
+
+                String[] path = file.FileName.Split('\\');
+                lbName.Text = "Archivo: " + path[path.Length-1];
+                lbName.Visible = true;
+                btAction.Visible = true;
+            }
+        }
+    }
+}
